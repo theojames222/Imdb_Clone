7883c5fa149d48019f7b2eb984825e0b3db75d7b
theojames222 <james.theo222@gmail.com>
2024-03-20 10:51:19 -0400

    add suspense to navbaritem

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,21 +1,24 @@
 "use client";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 export default function NavbarItem({ title, param }) {
   const searchParams = useSearchParams();
   const genre = searchParams.get("genre");
   return (
-    <div>
-      <Link
-        href={`/?genre=${param}`}
-        className={`hover:text-amber-600 font-semibold ${
-          genre === param
-            ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
-            : ""
-        }`}
-      >
-        {title}
-      </Link>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        <Link
+          href={`/?genre=${param}`}
+          className={`hover:text-amber-600 font-semibold ${
+            genre === param
+              ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+              : ""
+          }`}
+        >
+          {title}
+        </Link>
+      </div>
+    </Suspense>
   );
 }
