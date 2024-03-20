4ea1ca1d53eef36a2daad92b75f6fca40482495c
theojames222 <james.theo222@gmail.com>
2024-03-20 16:32:51 -0400

    fixed api call

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Results from "@/components/Results";
 const API_KEY = process.env.API_KEY;
 import { Suspense } from "react";
 
-export default async function Home({ params }) {
-  const genre = params.genre || "fetchTrending";
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/all/week"
